38937ac43f9b3684f0c8989c5e95c45ece9811eb
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-11-28 18:55:35 +0100

    fix: update readme + global

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,21 +1,21 @@
-// comment this part and uncomment the one below
+// uncomment this part and comment the one below, if you want the default behavior
 
-export default function () {
-  return { x: 0, y: 0, behavior: 'smooth' }
-}
-
-// uncomment the part below if you want to define globally available scrolling behaviors
-
-// import Vue from 'vue'
-// import VueScrollTo from 'vue-scrollto'
-// Vue.use(VueScrollTo)
-
-// export default function (to, from, savedPosition) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (to.hash === '#scroll-here') {
-//         VueScrollTo.scrollTo('#scroll-here', 300, { easing: 'linear' })
-//       }
-//     }, 10)
-//   })
+// export default function () {
+//   return { x: 0, y: 0, behavior: 'smooth' }
 // }
+
+// comment the part below if you do not want to define globally available scrolling behavior
+
+import Vue from 'vue'
+import VueScrollTo from 'vue-scrollto'
+Vue.use(VueScrollTo)
+
+export default function (to, from, savedPosition) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (to.hash === '#scroll-here') {
+        VueScrollTo.scrollTo('#scroll-here', 300, { easing: 'linear' })
+      }
+    }, 10)
+  })
+}
